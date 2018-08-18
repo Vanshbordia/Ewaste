3ed8b288522a48e41e8ddbf798af5e1df59b6d82
vanshbordia <vanshbordia@gmail.com>
2018-08-18 18:06:02 +0530

    3.0

--- app/src/main/java/com/vansh/ewaste/Image.java
+++ app/src/main/java/com/vansh/ewaste/Image.java
@@ -1,14 +1,13 @@
 package com.vansh.ewaste;
 
-import android.app.DownloadManager;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
