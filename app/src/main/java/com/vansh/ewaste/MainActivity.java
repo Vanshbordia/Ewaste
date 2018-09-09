c8246843197170cfb562763c0eb9f31589ff7cf0
vanshbordia <vanshbordia@gmail.com>
2018-09-09 10:05:17 +0530

    3.1.0

--- app/src/main/java/com/vansh/ewaste/MainActivity.java
+++ app/src/main/java/com/vansh/ewaste/MainActivity.java
@@ -2,12 +2,17 @@ package com.vansh.ewaste;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity{
     Firebase firebase;
 
     DatabaseReference databaseReference;
+    View view;
 
     // Root Database Name for Firebase Database.
     public static final String Database_Path = "ewaste-a89f2";
@@ -49,6 +55,22 @@ public class MainActivity extends AppCompatActivity{
 
         AddressEditText = findViewById(R.id.address);
 
+        PhoneNumberEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+
+                } else {
+                    if (PhoneNumberEditText.getText().toString().length() <=9) {
+                        PhoneNumberEditText.setError("Please enter your number correctly");
+                    } else {
+                        // your code here
+                        PhoneNumberEditText.setError(null);
+                    }
+                }
+
+            }
+        });
 
 
 
