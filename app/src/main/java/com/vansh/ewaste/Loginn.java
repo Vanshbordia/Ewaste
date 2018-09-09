4812d226bfb5b72317cd99adb7bf3e0383d67170
vanshbordia <vanshbordia@gmail.com>
2018-09-09 11:13:46 +0530

    3.1.1

---
+++ app/src/main/java/com/vansh/ewaste/Loginn.java
@@ -0,0 +1,168 @@
+package com.vansh.ewaste;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Loginn extends AppCompatActivity {
+    Button myButton;
+    View myView;
+    View myView1;
+    boolean isUp;
+    public EditText loginEmailId, logInpasswd;
+    Button btnLogIn;
+    TextView signup;
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginn);
+
+        myView = findViewById(R.id.my_view);
+        myView1 = findViewById(R.id.my_view1);
+        myButton = findViewById(R.id.my_button);
+        firebaseAuth = FirebaseAuth.getInstance();
+        loginEmailId = findViewById(R.id.loginEmail);
+        logInpasswd = findViewById(R.id.loginpaswd);
+        btnLogIn = findViewById(R.id.btnLogIn);
+        signup = findViewById(R.id.TVSignIn);
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(Loginn.this, "User logged in ", Toast.LENGTH_SHORT).show();
+                    Intent I = new Intent(Loginn.this, Selection.class);
+                    startActivity(I);
+                } else {
+                    Toast.makeText(Loginn.this, "Login to continue", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(Loginn.this, ActivitySignup.class);
+                startActivity(I);
+            }
+        });
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userEmail = loginEmailId.getText().toString();
+                String userPaswd = logInpasswd.getText().toString();
+                if (userEmail.isEmpty()) {
+                    loginEmailId.setError("Provide your Email first!");
+                    loginEmailId.requestFocus();
+                } else if (userPaswd.isEmpty()) {
+                    logInpasswd.setError("Enter Password!");
+                    logInpasswd.requestFocus();
+                } else if (userEmail.isEmpty() && userPaswd.isEmpty()) {
+                    Toast.makeText(Loginn.this, "Fields Empty!", Toast.LENGTH_SHORT).show();
+                } else if (!(userEmail.isEmpty() && userPaswd.isEmpty())) {
+                    firebaseAuth.signInWithEmailAndPassword(userEmail, userPaswd).addOnCompleteListener(Loginn.this, new OnCompleteListener() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(Loginn.this, "Not sucessfull", Toast.LENGTH_SHORT).show();
+                            } else {
+                                startActivity(new Intent(Loginn.this, Selection.class));
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(Loginn.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+
+        // initialize as invisible (could also do in xml)
+
+        myView1.setVisibility(View.INVISIBLE);
+        myButton.setText("Login");
+        isUp = false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+    // slide the view from below itself to the current position
+    public void slideUp(View view) {
+        view.setVisibility(View.VISIBLE);
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                view.getHeight(),  // fromYDelta
+                0);                // toYDelta
+        animate.setDuration(500);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
+
+    // slide the view from its current position to below itself
+    public void slideDown(View view) {
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                0,                 // fromYDelta
+                view.getHeight()); // toYDelta
+        animate.setDuration(500);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+    }
+
+
+
+    @SuppressLint("SetTextI18n")
+    public void onSlideViewButtonClick(View view) {
+        if (isUp) {
+            slideDown(myView);
+            slideUp(myView1);
+
+            myButton.setText("Signup");
+        } else {
+            slideDown(myView1);
+            slideUp(myView);
+            myButton.setText("Login");
+        }
+        isUp = !isUp;
+    }
+
+}
